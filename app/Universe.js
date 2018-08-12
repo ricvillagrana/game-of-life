075577e94c827c3f84bd0f3a07c65c36e558c14b
Ricardo Villagrana <rikrc.d@gmail.com>
2018-08-11 23:08:26 -0500

    [IMP] Add better view in console

--- app/Universe.js
+++ app/Universe.js
@@ -2,12 +2,30 @@
 const Cell = require('./Cell');
 const grid = require('../data/grids/empty');
 
+
+
 // Creating Universe
 const Universe = function () {
     this.grid = grid;
-
+    
     this.show = () => {
+        let output = ""
+        grid.forEach(row => {
+            row.forEach(element => {
+                if (element == 1) output += "██";
+                else output += "   ";
+            });
+            output += "\n";
+        });
+        console.log(output);
+    }
+
+    this.show_binary = () => {
         console.log(this.grid);
+    }
+
+    this.getGrid = () => {
+        return this.grid;
     }
 }
 
